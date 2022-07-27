39c7646501602821f8e5d52ae1dd49c0926b8815
newbie1221 <67160026+newbie1221@users.noreply.github.com>
2022-07-27 20:38:02 +0530

    Add files via upload

---
+++ assignment5/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace assignment5
+{
+
+    class Accounts {
+        public int accountno;
+        public string custname;
+        public string acctype;
+        public char ttype;
+        public int amount;
+        public int balance=1000;
+        public void show() {
+            Console.WriteLine($"Account no: {accountno}, Custname:{custname}, acctype:{acctype}, Balance:{balance}");
+        }
+    }
+    class check0
+        {
+        Accounts accounts=new Accounts();
+        
+        public void check(string ttype, int amt)
+        {
+            if (ttype == "w")
+                debit(amt);
+            else
+                credit(amt);
+        }
+        public void debit(int amt) {
+            accounts.balance = accounts.balance - amt;
+            Console.WriteLine($"Account no: {accounts.accountno}, Custname:{accounts.custname}, acctype:{accounts.acctype}, Transaction:w, Amount:{amt}, Balance:{accounts.balance}");
+            
+        }
+        public void credit(int amt)
+        {
+            accounts.balance = accounts.balance + amt;
+            Console.WriteLine($"Account no: {accounts.accountno}, Custname:{accounts.custname}, acctype:{accounts.acctype}, Transaction:d, Amount:{amt}, Balance:{accounts.balance}");
+        }
+        public void accept(int acno,string cusname,string acctype) {
+            accounts.accountno = acno;
+            accounts.custname = cusname;
+            accounts.acctype = acctype;
+        }
+        public Accounts show() {
+            return accounts;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            check0 c1=new check0();
+            c1.accept(1815001, "Debopriyo", "Savings");
+            
+            Accounts accounts=new Accounts();
+            accounts = c1.show();
+            accounts.show();
+            c1.check("w", 540);
+            accounts.show();
+            c1.check("f", 500);
+            accounts = c1.show();
+            accounts.show();
+        }
+    }
+}
---
+++ assignment5/task1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    class task1
+    {
+        public int rollno;
+        public string name;
+        public int clas;
+        public int SEM;
+        public string branch;
+        public int[] marks=new int[5];
+    }
+    class task2 {
+        public task1 student = new task1();
+        
+        public task2(int roll, string name, int clas, int sem, string branch, params int[] x)
+        {
+            
+                student.rollno = roll;
+                student.name=name;
+                student.clas = clas;
+                student.SEM = sem;
+                student.branch = branch;
+                for(int i=0;i<5;i++)
+                    student.marks[i] = x[i];
+        }
+        public void result() {
+            
+                    int avg = 0,flag=0;
+                    for (int j = 0; j < 5; j++) {
+                        avg += student.marks[j];
+                        if(student.marks[j]<35)
+                            flag = 1;
+                    }
+                    avg /= 5;
+                    if (avg < 50)
+                        flag = 1;
+                    if (flag == 1)
+                        Console.WriteLine($"Name: {student.name}, Average: {avg} , Status: Failed");
+                    else
+                        Console.WriteLine($"Name: {student.name}, Average: {avg} , Status: Passed");
+
+              
+        }
+
+        public void display() {
+            
+                Console.WriteLine($"Rollno: {student.rollno}, Name: {student.name}, Branch: {student.branch}, Semester: {student.SEM}, Class: {student.clas}");
+            
+        }
+    }
+    class proxy {
+        static void Main() {
+            task2 t = new task2(1815001,"Debopriyo",5,6,"CSE",70,80,90,90,50);
+            t.display(); //for displaying details of student
+            t.result(); //for checking result of student
+        }
+    }
+}
